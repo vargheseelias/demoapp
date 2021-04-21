baa40d7049fe090c7610ce15f1f6512f07cad262
Your Name <you@example.com>
2021-04-21 12:33:21 +0530

    demo app created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import{FormBuilder,ReactiveFormsModule} from '@angular/forms'
+ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { from } from 'rxjs';
+import{HttpClientModule} from '@angular/common/http';
+import { UserhomeComponent } from './userhome/userhome.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserhomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/dataservice.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataserviceService {
+
+  constructor(private http:HttpClient) { }
+
+register(uid:any,uname:any,pswd:any){
+  const data={
+    uid,
+    uname,
+    pswd,
+  }
+return this.http.post('http://localhost:3000/register',data)
+}
+
+login(uid:any,pswd:any){
+  const data={
+    uid,
+    pswd,
+  }
+  return this.http.post('http://localhost:3000/login',data)
+}
+
+
+}
